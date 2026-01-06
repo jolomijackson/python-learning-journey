27366ffcc04080fbad384beb26fd407f44eb6545
jolomijackson <oritsejolomijackson@gmail.com>
2026-01-06 19:31:56 +0100

    Create week1_basics.py

---
+++ basics/week1_basics.py
@@ -0,0 +1,8 @@
+# Week 1 Python basics
+name = "Jolomi"
+age = 18
+print("Hekko World!")
+print("My name is ", name, "and I am learning to use Python!")
+print("I am ", age, "years old and going to study computer science.")
+age += 5
+print("I will be ", age, "years old in a few months.")
