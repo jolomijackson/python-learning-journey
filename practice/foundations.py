758e125b04a7592c4f9bedb313e6ead299281949
jolomijackson <oritsejolomijackson@gmail.com>
2026-01-07 13:25:13 +0100

    Update and rename week1_basics.py to foundations.py

--- practice/foundations.py
+++ practice/foundations.py
@@ -1,8 +1,17 @@
-# Week 1 Python basics
+# Python basics
+
+# Introduction
 name = "Jolomi"
 age = 18
-print("Hekko World!")
+print("Hello World!")
 print("My name is ", name, "and I am learning to use Python!")
 print("I am ", age, "years old and going to study computer science.")
 age += 5
 print("I will be ", age, "years old in a few months.")
+
+# Hour to minute converter
+print("Hour to minute converter!")
+hours = float(input("Hours: "))
+minute = hours * 60
+print("Minutes: ", minute)
+
