94b3b47489b6dd72054ae98742d63b5d44a68962
jolomijackson <oritsejolomijackson@gmail.com>
2026-01-26 12:58:32 +0100

    Remove add_greetings function and its usage

--- practice/functions_practice.py
+++ practice/functions_practice.py
@@ -1,9 +1,1 @@
-# Adding greetings to names in a list
-def add_greetings(names):
-  greeting = []
-  for name in names:
-    greeting.append("Hello, " + name)
-  return greeting
-
-print(add_greetings(["Owen", "Max", "Sophie"]))
 
