44cec5e426381e75f2e26785eee25c4f74c07a5d
jolomijackson <oritsejolomijackson@gmail.com>
2026-01-29 20:29:51 +0100

    Implement reversed_list function
    Add function to check if two lists are reversed

--- practice/loops.py
+++ practice/loops.py
@@ -112,3 +112,13 @@ def same_values(lst1, lst2):
   return matching_index
 
 print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
+
+# Reversed list
+def reversed_list(lst1, lst2):
+  for index in range(len(lst1)):
+    if lst1[index] != lst2[len(lst2) - 1 - index]:
+      return False
+  return True
+
+print(reversed_list([1, 2, 3], [3, 2, 1]))
+print(reversed_list([1, 5, 3], [3, 2, 1]))
