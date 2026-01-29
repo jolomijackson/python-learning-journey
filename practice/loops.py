3808429305c1ffab5ff98a088d10f1c1d76e2ccf
jolomijackson <oritsejolomijackson@gmail.com>
2026-01-29 17:37:04 +0100

    Add function to find matching indices in two lists

--- practice/loops.py
+++ practice/loops.py
@@ -102,3 +102,13 @@ def max_num(nums):
   return max
 
 print(max_num([50, -10, 0, 75, 20]))
+
+# Matching indices
+def same_values(lst1, lst2):
+  matching_index = []
+  for index in range(len(lst1)):
+    if lst1[index] == lst2[index]:
+      matching_index.append(index)
+  return matching_index
+
+print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
