a676cff26f9224cbe3c4dd5e65003a830a9a667a
jolomijackson <oritsejolomijackson@gmail.com>
2026-01-30 00:03:12 +0100

    Add account generator function for username creation

--- practice/strings.py
+++ practice/strings.py
@@ -1,1 +1,10 @@
+# Username using slicing
+first_name = "Julie"
+last_name = "Blevins"
 
+def account_generator(first_name, last_name):
+  username = first_name[:3] + last_name[:3]
+  return username
+
+new_account = account_generator("Julie", "Blevins")
+print(new_account)
