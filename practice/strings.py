53f3984c2c980fcd2fe7008523571b46bf097f21
jolomijackson <oritsejolomijackson@gmail.com>
2026-01-30 00:15:42 +0100

    Update strings.py

--- practice/strings.py
+++ practice/strings.py
@@ -8,3 +8,13 @@ def account_generator(first_name, last_name):
 
 new_account = account_generator("Julie", "Blevins")
 print(new_account)
+
+first_name = "Reiko"
+last_name = "Matsuki"
+
+def password_generator(first_name, last_name):
+  temp_password = first_name[len(first_name)-3:] + last_name[len(last_name)-3:]
+  return temp_password
+
+temp_password = password_generator("Reiko", "Matsuki")
+print(temp_password)
