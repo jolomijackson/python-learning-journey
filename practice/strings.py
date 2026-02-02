7addbb9c04fc60db1a061cec36846cf6fe440fc4
jolomijackson <oritsejolomijackson@gmail.com>
2026-02-02 15:00:53 +0100

    Enhance string splitting examples in strings.py
    Added examples for splitting strings using a for loop and escape character.

--- practice/strings.py
+++ practice/strings.py
@@ -106,6 +106,7 @@ line_one = "The sky has given over"
 line_one_words = line_one.split()
 print(line_one_words)
 
+## Splitting with index and for loop
 authors = "Audre Lorde,Gabriela Mistral,Jean Toomer,An Qi,Walt Whitman,Shel Silverstein,Carmen Boullosa,Kamala Suraiyya,Langston Hughes,Adrienne Rich,Nikki Giovanni"
 
 author_names = authors.split(",")
@@ -116,3 +117,26 @@ for name in author_names:
   name.split()[-1]
   author_last_names.append(name.split()[-1])
 print(author_last_names)
+
+## Splitting with escape character
+spring_storm_text = \
+"""The sky has given over 
+its bitterness. 
+Out of the dark change 
+all day long 
+rain falls and falls 
+as if it would never end. 
+Still the snow keeps 
+its hold on the ground. 
+But water, water 
+from a thousand runnels! 
+It collects swiftly, 
+dappled with black 
+cuts a way for itself 
+through green ice in the gutters. 
+Drop after drop it falls 
+from the withered grass-stems 
+of the overhanging embankment."""
+
+spring_storm_lines = spring_storm_text.split("\n")
+print(spring_storm_lines)
