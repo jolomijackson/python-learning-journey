983558a057dab1184b88d65f78d15faee7e782ca
jolomijackson <oritsejolomijackson@gmail.com>
2026-04-15 09:08:37 +0100

    Update and rename cryptography.py to caeser_cipher.py

--- project/caeser_cipher.py
+++ project/caeser_cipher.py
@@ -1,4 +1,5 @@
-## Ceaser Cipher
+"""Ceaser Cipher with 2 methods
+Attempt 1"""
 # Decoding function
 def caeser_decode(message, offset):
     words = message.split(" ")
@@ -53,43 +54,36 @@ for num in range(0, 31):
     decoded = caeser_decode("vhfinmxkl atox kxgwxkxw tee hy maxlx hew vbiaxkl hulhexmx. px'ee atox mh kxteer lmxi ni hnk ztfx by px ptgm mh dxxi hnk fxlltzxl ltyx.", num)
     print("Offset {}: {}".format(num, decoded))
 
-## Vigenere Cipher
-# Decoder
-def decoder(message, keyword):
-    alphabet = list("abcdefghijklmnopqrstuvwxyz")
-    full_keyword = (keyword * ((len(message) // len(keyword) + 1)))[:len(message)]
-    decoded_words = []
-    keyword_index = 0
-    for word in message.split(" "):
-        decoded_word = []
-        for letter in word:
-            if letter in alphabet:
-                k = full_keyword[keyword_index]
-                decoded_letter = alphabet[(alphabet.index(letter) + alphabet.index(k)) % 26]
-                decoded_word.append(decoded_letter)
-                keyword_index += 1
-            else:
-                decoded_word.append(letter)
-        decoded_words.append("".join(decoded_word))
-    return " ".join(decoded_words)
-print(decoder("txm srom vkda gl lzlgzr qpdb? fepb ejac! ubr imn tapludwy mhfbz cza ruxzal wg zztcgcexxch!", "friends"))
+
+"""Attempt 2"""
+import string
 
 # Encoder
-def encoder(message, keyword):
-    alphabet = list("abcdefghijklmnopqrstuvwxyz")
-    full_keyword = (keyword * ((len(message) // len(keyword) + 1)))[:len(message)]
-    decoded_words = []
-    keyword_index = 0
-    for word in message.split(" "):
-        decoded_word = []
-        for letter in word:
-            if letter in alphabet:
-                k = full_keyword[keyword_index]
-                decoded_letter = alphabet[(alphabet.index(letter) - alphabet.index(k)) % 26]
-                decoded_word.append(decoded_letter)
-                keyword_index += 1
-            else:
-                decoded_word.append(letter)
-        decoded_words.append("".join(decoded_word))
-    return " ".join(decoded_words)
-print(encoder("your cryptography really stressed me out! i was overthinking a lot of things but i now see that it was all unnecessary.", "friends"))
+def caeser_encrypt(message, offset):
+    alphabet = string.ascii_lowercase
+    new_letters = []
+    for letter in message:
+        if not letter in alphabet:
+            new_letters.append(letter)
+        else:
+            letter_index = alphabet.index(letter)
+            new_index = letter_index + offset
+            new_letter = alphabet[new_index % 26]
+            new_letters.append(new_letter)
+    new_message = ''.join(new_letters)
+    return new_message
+
+# Decoder
+def caeser_decrypt(message, offset):
+    alphabet = string.ascii_lowercase
+    new_letters = []
+    for letter in message:
+        if not letter in alphabet:
+            new_letters.append(letter)
+        else:
+            letter_index = alphabet.index(letter)
+            new_index = letter_index - offset
+            new_letter = alphabet[new_index % 26]
+            new_letters.append(new_letter)
+    new_message = ''.join(new_letters)
+    return new_message
